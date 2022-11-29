65c8d7f599dec3f02dcb2809891cd32d8a5e8bcc
pk4812 <pk4@terpmail.umd.edu>
2022-11-29 11:31:02 -0500

    Create Group Final Project.py

---
+++ Group Final Project.py
@@ -0,0 +1,8 @@
+""" 
+Names:      Sarah Adel, Richard Forng, Philemon Kim, Paul Sung
+Assignment: Final Project
+Due Date:   December 15th (8:00pm), 2022
+INST326-ESG1 William S. Farmer Fall 2022
+"""
+
+#TEST
