22ee50057f6ce876bfff171dc2a95657c9816803
pk4812 <pk4@terpmail.umd.edu>
2022-11-29 11:50:09 -0500

    Update Group Final Project.py

--- Group Final Project.py
+++ Group Final Project.py
@@ -5,5 +5,27 @@ Due Date:   December 15th (8:00pm), 2022
 INST326-ESG1 William S. Farmer Fall 2022
 """
 
-#TEST
-#Richard test commit
+import sys
+import argparse
+import pandas as pd
+
+
+
+def parse_args(args_list):
+    """
+    Comments:
+        Takes a list of strings from the command prompt and passes them through as arguments
+    Args:
+        args_list (list) : the list of strings from the command prompt
+    Returns:
+        args (ArgumentParser)
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type = str, help = 'The name of the file wth the contact data')
+    args = parser.parse_args(args_list)
+
+    return args
+
+if __name__ == "__main__":
+
+    args = parse_args(sys.argv[1:])
