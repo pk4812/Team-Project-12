91af2d36d957ebd0bbb66e6058ee39f444d84626
pk4812 <pk4@terpmail.umd.edu>
2022-12-13 13:48:16 -0500

    Working Iteration
    Revamped old code to match existing final project requirements.

--- gfp.py
+++ gfp.py
@@ -9,10 +9,7 @@ import sys
 import argparse
 import pandas as pd
 
-#creates a dataframe that takes in data from a .csv file and will give them headers.
-df = pd.read_csv("contacts.csv", names = ["Names", "Emails", "Phone Numbers"])
-
-def determine_favorites(favorites_input):
+def contacts_picker(input, df):
     """
     Comments:
         Takes input from user and creates a favorites contact group, using the dataframe.
@@ -22,82 +19,34 @@ def determine_favorites(favorites_input):
         
     """
     
-    chosen_favorites = df.query('Names.str.startswith(@favorites_input)', engine = "python")
-    print(chosen_favorites)
+    chosen_contacts = df.query('Names.str.startswith(@input)', engine = "python")
+
+    return(chosen_contacts)
+
+def print_menu():
+
+    print(
+    """
+    -------------------------------------------
+    \t\tCREATING CONTACTS LIST
+    -------------------------------------------
+    Please choose one of the following options:
+    1. Create a Favorites Group
+    2. Create a Emergency Contacts Group
+    3. Create a Work Group
+    4. Create a Education Group
+    5. Create a Hobbies Group
+    6. Create a International Group
+    7. Exit Program"""
+    )
+
+def choice():
     
-    pass
-'''
-def determine_emer_con_list(emer_con_list_input):
-    """
-    Comments:
-        Takes input from user and creates an emergency contact list group, using the dataframe.
-    Parameters:
-        
-    Returns:
-        
-    """
+    choice = int(input("\nPlease enter your choice: "))
 
-    
+    return choice
 
-    pass
-
-def determine_work(work_input):
-    """
-    Comments:
-        Takes input from user and creates a work contact group, using the dataframe.
-    Parameters:
-        
-    Returns:
-        
-    """
-
-
-    
-    pass
-
-def determine_education(education_input):
-    """
-    Comments:
-        Takes input from user and creates an education contact group, using the dataframe.
-    Parameters:
-        
-    Returns:
-        
-    """
-
-
-    
-    pass
-
-def determine_hobbies(hobbies_input):
-    """
-    Comments:
-        Takes input from user and creates a hobbies contact group, using the dataframe.
-    Parameters:
-        
-    Returns:
-        
-    """
-
-
-    
-    pass
-
-def determine_international(international_input):
-    """
-    Comments:
-        Takes input from user and creates a international contacts group, using the dataframe.
-    Parameters:
-        
-    Returns:
-        
-    """
-
-
-    
-    pass
-'''
-def main(filename):
+def main(filename:str):
     """
     Comments:
         Will ask for certain inputs from user, as arguments, to create certain groups, using the dataframe.  
@@ -106,26 +55,68 @@ def main(filename):
     Returns:
         
     """
-    favorites_input = list(input("Enter your Favorite contacts: ").split(","))
-    '''
-    emer_con_list_input = input("Enter your Emergency Contacts: ")
-    work_input = input("Enter your Work related Contacts: ")
-    education_input = input("Enter your School related Contacts: ")
-    hobbies_input = input("Enter your Hobby Contacts: ")
-    international_input = input("Enter your International Contacts: ")
-    '''
 
-    favorites = determine_favorites(favorites_input)
-    '''
-    emer_con_list = determine_emer_con_list(emer_con_list_input)
-    work = determine_work(work_input)
-    education = determine_education(education_input)
-    hobbies = determine_hobbies(hobbies_input)
-    international = determine_international(international_input)
-    '''
-    print(df)
+    #creates a dataframe that takes in data from a .csv file and will give them headers.
+    df = pd.read_csv(filename, names = ["Names", "Emails", "Phone Numbers"])
 
-    pass
+    #Calls the printed menu function
+    print_menu()
+
+    #Receives inputs from the command line
+    menu_choice = 1
+    methods_ran = []
+    while menu_choice in (1, 2, 3, 4, 5, 6, 7):
+        menu_choice = choice()
+        if menu_choice == 1:
+            favorites_input = tuple(input("Enter your Favorite contacts: ").split(", "))
+            favorites = contacts_picker(favorites_input, df)
+            methods_ran.append(1)
+
+        elif menu_choice == 2:
+            emer_con_list_input = tuple(input("Enter your Emergency Contacts: ").split(", "))
+            emer_con_list = contacts_picker(emer_con_list_input, df)
+            methods_ran.append(2)
+
+        elif menu_choice == 3:
+            work_input = tuple(input("Enter your Work related Contacts: ").split(", "))
+            work = contacts_picker(work_input, df)
+            methods_ran.append(3)
+
+        elif menu_choice == 4:
+            education_input = tuple(input("Enter your School related Contacts: ").split(", "))
+            education = contacts_picker(education_input, df)
+            methods_ran.append(4)
+
+        elif menu_choice == 5:
+            hobbies_input = tuple(input("Enter your Hobby Contacts: ").split(", "))
+            hobbies = contacts_picker(hobbies_input, df)
+            methods_ran.append(5)
+
+        elif menu_choice == 6:
+            international_input = tuple(input("Enter your International Contacts: ").split(", "))
+            international = contacts_picker(international_input, df)
+            methods_ran.append(6)
+
+        elif menu_choice == 7:
+
+            methods_ran.append(7)
+            methods_ran.sort()
+
+            for method in methods_ran:
+                if method == 1:
+                    print("\nHere are your Favorites Contacts: \n", favorites)
+                elif method == 2:
+                    print("\nHere are your Emergency Contacts Contacts: \n", emer_con_list)
+                elif method == 3:
+                    print("\nHere are your Work Contacts: \n", work)
+                elif method == 4:
+                    print("\nHere are your Education Contacts: \n", education)
+                elif method == 5:
+                    print("\nHere are your Hobbies Contacts: \n", hobbies)
+                elif method == 6:
+                    print("\nHere are your International Contacts: \n", international)
+                elif method == 7:
+                    sys.exit("\nGoodbye.")
 
 def parse_args(args_list):
     """
@@ -138,12 +129,6 @@ def parse_args(args_list):
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', type = str, help = 'The name of the file wth the contact data')
-    #parser.add_argument('favorites', type = str, help = 'The names of the users favorite contacts')
-    #parser.add_argument('emergency', type = str, help = 'The names of the users emergency contacts')
-    #parser.add_argument('work', type = str, help = 'The name of the users work contacts')
-    #parser.add_argument('education', type = str, help = 'The name of the users school contacts')
-    #parser.add_argument('hobbies', type = str, help = 'The name of the users hobby contact')
-    #parser.add_argument('international', type = str, help = 'The name of the users international contacts')
     args = parser.parse_args(args_list)
 
     return args
